e373c64fa52bd0a65dc051e9a650bfd18f0193bd
Chris Peterson <cdpeters1@gmail.com>
2022-11-29 19:20:52 -0800

    Add environment and dev tools config files

---
+++ dev_tools.sh
@@ -0,0 +1,19 @@
+Cyan='\033[0;36m'
+BCyan='\033[1;36m'
+BWhite='\033[1;37m'
+Color_Off='\033[0m'
+
+echo -e "${Cyan}============== ${BCyan}DEV TOOLS${Cyan} ==============${Color_Off}"
+echo -e "${Cyan}Running ${BCyan}black${Cyan} code formatter:${Color_Off}"
+
+black .
+
+echo
+echo -e "${Cyan}Running ${BCyan}isort${Cyan} import formatter:${Color_Off}"
+
+isort .
+
+echo
+echo -e "${Cyan}Running ${BCyan}flake8${Cyan} code linter:${Color_Off}"
+
+flake8 .

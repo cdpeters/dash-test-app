5a0581a277055bbfb1bd153a36493f9e9613eb87
Chris Peterson <cdpeters1@gmail.com>
2022-12-08 07:44:11 -0800

    Add interrogate to dev_tools.sh and adjust its config for local and pre-commit hook use

--- dev_tools.sh
+++ dev_tools.sh
@@ -3,7 +3,8 @@ BCyan='\033[1;36m'
 BWhite='\033[1;37m'
 Color_Off='\033[0m'
 
-echo -e "${Cyan}============== ${BCyan}DEV TOOLS${Cyan} ==============${Color_Off}"
+echo -e "${Cyan}=================== ${BCyan}DEV TOOLS${Cyan} ===================${Color_Off}"
+echo
 echo -e "${Cyan}Running ${BCyan}black${Cyan} code formatter:${Color_Off}"
 
 black .
@@ -16,4 +17,12 @@ isort .
 echo
 echo -e "${Cyan}Running ${BCyan}flake8${Cyan} code linter:${Color_Off}"
 
+interrogate .
+
+echo
+echo -e "${Cyan}Running ${BCyan}flake8${Cyan} code linter:${Color_Off}"
+
 flake8 .
+
+echo
+echo -e "${Cyan}============== ${BCyan}DEV TOOLS COMPLETED${Cyan} ==============${Color_Off}"

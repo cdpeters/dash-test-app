ecb44e321f6b56d75ff0f4ff25fe941089c64eb2
Chris Peterson <cdpeters1@gmail.com>
2022-12-09 07:31:34 -0800

    Move navbar callback to navbar file, restructure navbar functions

--- src/app.py
+++ src/app.py
@@ -2,16 +2,10 @@
 
 import dash
 import dash_bootstrap_components as dbc
-from dash import Dash, Input, Output, State
+from dash import Dash
 
-from components.navbar import navbar
-from utils.constants import (
-    DATA_COLLAB_LOGO,
-    DBC_CSS,
-    NAVBAR_COLLAPSE,
-    NAVBAR_TOGGLER,
-    THEME,
-)
+from components.navbar import create_navbar_component
+from utils.constants import DATA_COLLAB_LOGO, DBC_CSS, THEME
 
 # Creates app, sets external stylesheets, and configures the app to be multi-page.
 app = Dash(
@@ -24,28 +18,15 @@ app = Dash(
 # Place the navbar and the `page_container` for the location of page contents
 app.layout = dbc.Container(
     [
-        navbar(page_registry=dash.page_registry, brand_logo=DATA_COLLAB_LOGO),
+        create_navbar_component(
+            page_registry=dash.page_registry, brand_logo=DATA_COLLAB_LOGO
+        ),
         # Location for page contents.
         dash.page_container,
     ],
     class_name="dbc px-0 vh-100",
     fluid=True,
 )
-
-
-# A callback for toggling the collapsible menu of page links when the screen size is
-# small.
-@app.callback(
-    Output(NAVBAR_COLLAPSE, "is_open"),
-    Input(NAVBAR_TOGGLER, "n_clicks"),
-    State(NAVBAR_COLLAPSE, "is_open"),
-)
-def toggle_navbar_collapse(n_clicks, is_open):
-    """Toggle the collapse of the navbar page links dropdown at small screen sizes."""
-    if n_clicks:
-        return not is_open
-    return is_open
-
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- src/components/navbar.py
+++ src/components/navbar.py
@@ -3,13 +3,20 @@
 The function `navbar` returns a Dash bootstrap components Navbar.
 """
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import Input, Output, State, callback, html
 
 from utils.constants import NAVBAR_COLLAPSE, NAVBAR_TOGGLER
 
+if TYPE_CHECKING:
+    from collections import OrderedDict
 
-def navbar(page_registry, brand_logo):
+
+def create_navbar_component(page_registry: OrderedDict, brand_logo: str) -> dbc.Navbar:
     """Create a navbar component with links to each page and branding.
 
     `page_registry` page data is used to construct NavLink components to link to the
@@ -29,8 +36,46 @@ def navbar(page_registry, brand_logo):
         Navbar component with page links.
 
     """
-    # `nav` is a container that holds the page links.
-    page_links = dbc.Nav(
+    return dbc.Navbar(
+        dbc.Container(
+            [
+                # Branding NavLink
+                dbc.NavLink(
+                    create_brand_component(brand_logo), href="/", class_name="ps-0"
+                ),
+                # Button to show/hide collapse component with page links when screen
+                # size is small.
+                dbc.NavbarToggler(id=NAVBAR_TOGGLER, n_clicks=0),
+                # Add `nav` to the collapse component. Visibility is controlled via the
+                # `is_open` argument.
+                dbc.Collapse(
+                    create_page_link_component(page_registry),
+                    id=NAVBAR_COLLAPSE,
+                    is_open=False,
+                    navbar=True,
+                ),
+            ],
+            class_name="dbc px-3",
+            fluid=True,
+        ),
+    )
+
+
+def create_page_link_component(page_registry: OrderedDict) -> dbc.Nav:
+    """Create page links for navigation.
+
+    Parameters
+    ----------
+    page_registry : OrderedDict
+        Registry of app pages to be used to construct page links on the navbar.
+
+    Returns
+    -------
+    dbc.Nav
+        Nav container with a NavLink for each page.
+    """
+    # `Nav` is a container that holds the page links.
+    return dbc.Nav(
         # Use list comprehension to create a NavLink for each page.
         [
             dbc.NavLink(
@@ -44,7 +89,21 @@ def navbar(page_registry, brand_logo):
         class_name="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
     )
 
-    brand = dbc.Row(
+
+def create_brand_component(brand_logo: str) -> dbc.Row:
+    """Create branding for a navbar.
+
+    Parameters
+    ----------
+    brand_logo : str
+        Path to the brand logo used in the navbar.
+
+    Returns
+    -------
+    dbc.Row
+        Row component containing the branding name and logo.
+    """
+    return dbc.Row(
         [
             # Brand logo.
             dbc.Col(html.Img(src=brand_logo, height="30px")),
@@ -55,24 +114,29 @@ def navbar(page_registry, brand_logo):
         class_name="g-0",
     )
 
-    return dbc.Navbar(
-        dbc.Container(
-            [
-                # Branding NavLink
-                dbc.NavLink(brand, href="/", class_name="ps-0"),
-                # Button to show/hide collapse component with page links when screen
-                # size is small.
-                dbc.NavbarToggler(id=NAVBAR_TOGGLER, n_clicks=0),
-                # Add `nav` to the collapse component. Visibility is controlled via the
-                # `is_open` argument.
-                dbc.Collapse(
-                    page_links,
-                    id=NAVBAR_COLLAPSE,
-                    is_open=False,
-                    navbar=True,
-                ),
-            ],
-            class_name="dbc px-3",
-            fluid=True,
-        ),
-    )
+
+# A callback for toggling the collapsible menu of page links when the screen size is
+# small.
+@callback(
+    Output(NAVBAR_COLLAPSE, "is_open"),
+    Input(NAVBAR_TOGGLER, "n_clicks"),
+    State(NAVBAR_COLLAPSE, "is_open"),
+)
+def toggle_navbar_collapse(n_clicks: int, is_open: bool) -> bool:
+    """Toggle the collapse of the navbar page links dropdown at small screen sizes.
+
+    Parameters
+    ----------
+    n_clicks : int
+        Number of clicks of the toggler.
+    is_open : bool
+        State of the navbar collapse component.
+
+    Returns
+    -------
+    bool
+        Update of the `is_open` variable.
+    """
+    if n_clicks:
+        return not is_open
+    return is_open

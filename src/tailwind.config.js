09a17069a33dac7d0212bf9654c0c13badc31dd0
Chris Peterson <cdpeters1@gmail.com>
2022-12-26 08:56:23 -0800

    Add tailwind to project

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{py,html,js}",
+    "./components/**/*.{py,html,js}",
+    "./data/**/*.{py,html,js}",
+    "./pages/**/*.{py,html,js}",
+    "./utils/**/*.{py,html,js}",
+    "./app.py"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

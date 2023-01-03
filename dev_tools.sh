4ec65b5735701ab8caa560282828f4e3f1fcc20c
Chris Peterson <cdpeters1@gmail.com>
2023-01-03 07:35:05 -0800

    Add mypy to dev_tools script

--- dev_tools.sh
+++ dev_tools.sh
@@ -1,28 +1,51 @@
-Cyan='\033[0;36m'
-BCyan='\033[1;36m'
-BWhite='\033[1;37m'
-Color_Off='\033[0m'
+#!/bin/bash
 
-echo -e "${Cyan}=================== ${BCyan}DEV TOOLS${Cyan} ===================${Color_Off}"
+# Get the name of the current directory
+current_dir=$(basename "$(pwd)")
+# Assign the names of the project and app directories
+project_dir="dash-test-app"
+app_dir="src"
+
+# Colors for printing
+cyan='\033[0;36m'
+bcyan='\033[1;36m'
+color_off='\033[0m'
+
+echo -e "${cyan}=================== ${bcyan}DEV TOOLS${cyan} ===================${color_off}"
 echo
-echo -e "${Cyan}Running ${BCyan}black${Cyan} code formatter:${Color_Off}"
+
+# Code formatter: black
+echo -e "${cyan}Running ${bcyan}black${cyan} (code formatter):${color_off}"
 
 black .
 
+# Import formatter: isort
 echo
-echo -e "${Cyan}Running ${BCyan}isort${Cyan} import formatter:${Color_Off}"
+echo -e "${cyan}Running ${bcyan}isort${cyan} (import formatter):${color_off}"
 
 isort .
 
+# Docstring Existence Checker: interrogate
 echo
-echo -e "${Cyan}Running ${BCyan}interrogate${Cyan} check docstrings exist:${Color_Off}"
+echo -e "${cyan}Running ${bcyan}interrogate${cyan} (check docstrings exist):${color_off}"
 
 interrogate .
 
+# Code linter: flake 8
 echo
-echo -e "${Cyan}Running ${BCyan}flake8${Cyan} code linter:${Color_Off}"
+echo -e "${cyan}Running ${bcyan}flake8${cyan} (code linter):${color_off}"
 
 flake8 .
 
+# Type checker: mypy
 echo
-echo -e "${Cyan}============== ${BCyan}DEV TOOLS COMPLETED${Cyan} ==============${Color_Off}"
+echo -e "${cyan}Running ${bcyan}mypy${cyan} (type checker):${color_off}"
+
+if [ $current_dir = $project_dir ]; then
+    mypy ./src --config-file="./pyproject.toml"
+elif [ $current_dir = $app_dir ]; then
+    mypy . --config-file="../pyproject.toml"
+fi
+
+echo
+echo -e "${cyan}============== ${bcyan}DEV TOOLS COMPLETED${cyan} ==============${color_off}"

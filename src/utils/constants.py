25218224ee6cf63710d08d38dcd8ae838f56c0bb
Chris Peterson <cdpeters1@gmail.com>
2023-01-04 08:11:54 -0800

    Adjust import for type checking and add icon and source code paths to constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -9,26 +9,28 @@ Constants:
         GOOGLE_DRIVE_DIR: Path to shared google drive collaboration folder.
         DATA_DIR: Path to the `data` directory in the project root (`dash-test-app`).
 
-    External Scripts:
-        TAILWIND_CDN: Script that adds support for tailwind css.
+    External Links:
+        APP_SOURCE_CODE_URL
 
-    Brand Logo:
+    Logos and Icons:
+        BACKGROUND_PAGE_ICON
+        DASHBOARD_PAGE_ICON
         DATA_COLLAB_LOGO
+        GITHUB_ICON
+        HOME_PAGE_ICON
 
     Component Ids:
         FIGURE_BAR
         FIGURE_TEMP
         FIGURE_PRECIP
         TABLE_CLIMATE
-        NAVBAR_TOGGLER
-        NAVBAR_COLLAPSE
 """
 
 from pathlib import Path
 
 import tomli
 
-# Data Directories ---------------------------------------------------------------------
+# Load secrets.toml --------------------------------------------------------------------
 
 # Use the file path (`__file__`) of this module to form the paths to both the google
 # drive collaboration folder and to the project's data directory. `parents[2]` is the
@@ -37,19 +39,25 @@ import tomli
 with open(Path(__file__).parents[2] / "secrets.toml", "rb") as f:
     secrets = tomli.load(f)
 
+
+# Data Directories ---------------------------------------------------------------------
+
 GOOGLE_DRIVE_DIR = Path(secrets["google_drive"]["path"])
 DATA_DIR = Path(__file__).parents[2] / "data"
 
 
-# External Scripts ---------------------------------------------------------------------
+# External Links -----------------------------------------------------------------------
 
-# Adds support for tailwind css.
-TAILWIND_CDN = "https://cdn.tailwindcss.com"
+APP_SOURCE_CODE_URL = "https://github.com/cdpeters/dash-test-app"
 
 
-# Brand Logo ---------------------------------------------------------------------------
+# Logos and Icons ----------------------------------------------------------------------
 
-DATA_COLLAB_LOGO = "/assets/data_collab.png"
+BACKGROUND_PAGE_ICON = "/assets/images/book.svg"
+DASHBOARD_PAGE_ICON = "/assets/images/chart-line.svg"
+DATA_COLLAB_LOGO = "/assets/images/data_collab.png"
+GITHUB_ICON = "/assets/images/github.svg"
+HOME_PAGE_ICON = "/assets/images/house.svg"
 
 
 # Component Ids ------------------------------------------------------------------------
@@ -58,5 +66,3 @@ FIGURE_BAR = "figure-bar"
 FIGURE_TEMP = "figure-temp"
 FIGURE_PRECIP = "figure-prcp"
 TABLE_CLIMATE = "table-01"
-NAVBAR_TOGGLER = "navbar-toggler"
-NAVBAR_COLLAPSE = "navbar-collapse"
